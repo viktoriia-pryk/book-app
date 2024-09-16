a3bb0b6685cb3e7c0c5d871ab9dedead7c51a3d0
Victoria <142920879+viktoriia9898989@users.noreply.github.com>
2024-09-16 17:32:47 +0300

    Initial React App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="app">
+      <header className="app-header">
+       <h1>Book Library App</h1>
+
+      </header>
+    </div>
+  )
+}
+
+export default App
