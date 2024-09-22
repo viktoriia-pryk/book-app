e3143586d48e22df79ede6945a553ecc55839ac0
Victoria <142920879+viktoriia9898989@users.noreply.github.com>
2024-09-22 17:48:37 +0300

    added errorSlice

---
+++ frontend/src/redux/slices/errorSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = ''
+
+const errorSlice = createSlice({
+    name: 'error',
+    initialState,
+    reducers: {
+        setError: (state, action) =>{
+            return action.payload
+        },
+        clearError: () =>{
+            return ''
+        }
+    }
+})
+
+export const { setError, clearError } = errorSlice.actions
+export const selectErrorMessage = (state) => state.error
+export default errorSlice.reducer
